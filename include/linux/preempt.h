2d93a817f5b4be2d2eb4c22354f6df8d7264c0f7
Thomas Gleixner <tglx@linutronix.de>
2011-03-21 13:32:17 +0100

    preempt-mark-legitimated-no-resched-sites.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/preempt.h
+++ include/linux/preempt.h
@@ -33,11 +33,13 @@ do { \
 	barrier(); \
 } while (0)
 
-#define preempt_enable_no_resched() \
+#define __preempt_enable_no_resched() \
 do { \
 	barrier(); \
 	dec_preempt_count(); \
 } while (0)
+
+#define preempt_enable_no_resched()	__preempt_enable_no_resched()
 
 #define preempt_check_resched() \
 do { \
@@ -83,6 +85,7 @@ do { \
 #else
 
 #define preempt_disable()		do { } while (0)
+#define __preempt_enable_no_resched()	do { } while (0)
 #define preempt_enable_no_resched()	do { } while (0)
 #define preempt_enable()		do { } while (0)
 #define preempt_check_resched()		do { } while (0)
