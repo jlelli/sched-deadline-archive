1fef837770be45969c3c9069595bf627cf34f58d
Peter Zijlstra <peterz@infradead.org>
2011-06-25 15:45:46 +0200

    sched-copy-cpumask-in-one-place.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched.c
+++ kernel/sched.c
@@ -5949,10 +5949,9 @@ void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 {
 	if (p->sched_class && p->sched_class->set_cpus_allowed)
 		p->sched_class->set_cpus_allowed(p, new_mask);
-	else {
-		cpumask_copy(&p->cpus_allowed, new_mask);
-		p->rt.nr_cpus_allowed = cpumask_weight(new_mask);
-	}
+
+	cpumask_copy(&p->cpus_allowed, new_mask);
+	p->rt.nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
 /*
