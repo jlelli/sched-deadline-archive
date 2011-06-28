74742eed42d145cee93d89760fd029909ecc81a3
Thomas Gleixner <tglx@linutronix.de>
2011-06-28 15:44:15 +0200

    softirq-thread-do-softirq.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2905,7 +2905,7 @@ int netif_rx_ni(struct sk_buff *skb)
 	preempt_disable();
 	err = netif_rx(skb);
 	if (local_softirq_pending())
-		do_softirq();
+		thread_do_softirq();
 	preempt_enable();
 
 	return err;
