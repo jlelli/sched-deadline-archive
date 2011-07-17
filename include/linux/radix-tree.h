1e43907b369a38a42ef99d702ea731295d28af2a
Thomas Gleixner <tglx@linutronix.de>
2011-07-17 21:33:18 +0200

    radix-tree-rt-aware.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/radix-tree.h
+++ include/linux/radix-tree.h
@@ -201,7 +201,13 @@ unsigned long radix_tree_next_hole(struct radix_tree_root *root,
 				unsigned long index, unsigned long max_scan);
 unsigned long radix_tree_prev_hole(struct radix_tree_root *root,
 				unsigned long index, unsigned long max_scan);
+
+#ifndef CONFIG_PREEMPT_RT_FULL
 int radix_tree_preload(gfp_t gfp_mask);
+#else
+static inline int radix_tree_preload(gfp_t gm) { return 0; }
+#endif
+
 void radix_tree_init(void);
 void *radix_tree_tag_set(struct radix_tree_root *root,
 			unsigned long index, unsigned int tag);
@@ -225,7 +231,7 @@ int radix_tree_tagged(struct radix_tree_root *root, unsigned int tag);
 
 static inline void radix_tree_preload_end(void)
 {
-	preempt_enable();
+	preempt_enable_nort();
 }
 
 #endif /* _LINUX_RADIX_TREE_H */
