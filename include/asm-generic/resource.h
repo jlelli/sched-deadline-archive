b051e41f2efc383ad7048c3a85da97c432e81b7a
Juri Lelli <juri.lelli@gmail.com>
2011-10-29 12:53:52 +0200

    sched: add resource limits for -deadline tasks.
    Add resource limits for non-root tasks in using the SCHED_DEADLINE
    policy, very similarly to what already exists for RT policies.
    
    In fact, this patch:
     - adds the resource limit RLIMIT_DLDLINE, which is the minimum value
       a user task can use as its own deadline;
     - adds the resource limit RLIMIT_DLRTIME, which is the maximum value
       a user task can use as it own runtime.
    
    Notice that to exploit these, a modified version of the ulimit
    utility and a modified resource.h header file are needed. They
    both will be available on the website of the project.
    
    Signed-off-by: Dario Faggioli <raistlin@linux.it>
    Signed-off-by: Juri Lelli <juri.lelli@gmail.com>

--- include/asm-generic/resource.h
+++ include/asm-generic/resource.h
@@ -45,7 +45,10 @@
 					   0-39 for nice level 19 .. -20 */
 #define RLIMIT_RTPRIO		14	/* maximum realtime priority */
 #define RLIMIT_RTTIME		15	/* timeout for RT tasks in us */
-#define RLIM_NLIMITS		16
+
+#define RLIMIT_DLDLINE		16	/* minimum deadline in us */
+#define RLIMIT_DLRTIME		17	/* maximum runtime in us */
+#define RLIM_NLIMITS		18
 
 /*
  * SuS says limits have to be unsigned.
@@ -87,6 +90,8 @@
 	[RLIMIT_NICE]		= { 0, 0 },				\
 	[RLIMIT_RTPRIO]		= { 0, 0 },				\
 	[RLIMIT_RTTIME]		= {  RLIM_INFINITY,  RLIM_INFINITY },	\
+	[RLIMIT_DLDLINE]	= { ULONG_MAX, ULONG_MAX },		\
+	[RLIMIT_DLRTIME]	= { 0, 0 },				\
 }
 
 #endif	/* __KERNEL__ */
