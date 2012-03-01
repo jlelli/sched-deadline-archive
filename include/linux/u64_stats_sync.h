8ef5472a0c7f96a8ed1e28c9a7e0fe60c82ef0a4
Thomas Gleixner <tglx@linutronix.de>
2012-03-01 16:16:02 +0100

    net: u64_stat: Protect seqcount
    On RT we must prevent that the writer gets preempted inside the write
    section. Otherwise a preempting reader might spin forever.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: stable-rt@vger.kernel.org
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/linux/u64_stats_sync.h
+++ include/linux/u64_stats_sync.h
@@ -70,6 +70,7 @@ struct u64_stats_sync {
 static inline void u64_stats_update_begin(struct u64_stats_sync *syncp)
 {
 #if BITS_PER_LONG==32 && defined(CONFIG_SMP)
+	preempt_disable_rt();
 	write_seqcount_begin(&syncp->seq);
 #endif
 }
@@ -78,6 +79,7 @@ static inline void u64_stats_update_end(struct u64_stats_sync *syncp)
 {
 #if BITS_PER_LONG==32 && defined(CONFIG_SMP)
 	write_seqcount_end(&syncp->seq);
+	preempt_enable_rt();
 #endif
 }
 
