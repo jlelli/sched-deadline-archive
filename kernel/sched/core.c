c1ef4903dbe8303036fbc943e5f2601c051c3488
Juri Lelli <juri.lelli@gmail.com>
2013-11-13 10:01:48 +0100

    Initialize memory before copying to userspace
    Reported-by: Steven Rostedt

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3779,7 +3779,6 @@ do_sched_setscheduler2(pid_t pid, int policy,
 	if (!param2 || pid < 0)
 		return -EINVAL;
 
-	memset(&lparam2, 0, sizeof(struct sched_param2));
 	if (copy_from_user(&lparam2, param2, sizeof(struct sched_param2)))
 		return -EFAULT;
 
@@ -3936,6 +3935,8 @@ SYSCALL_DEFINE2(sched_getparam2, pid_t, pid,
 
 	if (!param2 || pid < 0)
 		return -EINVAL;
+
+	memset(&lp, 0, sizeof(struct sched_param2));
 
 	rcu_read_lock();
 	p = find_process_by_pid(pid);
